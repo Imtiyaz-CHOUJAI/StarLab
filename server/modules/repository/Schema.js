8fa0979127b559439700fd55d05450aec8c18596
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2019-07-12 19:23:12 +0100

    WIP: define graphql schema

---
+++ server/modules/repository/Schema.js
@@ -0,0 +1,25 @@
+import {gql} from "apollo-server-express";
+
+const typeDefs = gql`
+  type Repository {
+    id: Int!
+    name: String!
+    full_name: String!
+    owner: Owner!
+    html_url: String!
+    description: String!
+    stargazers_count: Int!
+    open_issues: String!
+    created_at: String!
+  }
+  type Owner {
+    id: Int!
+    avatar_url: String!
+    login: String!
+  }
+  type Query {
+    repositories(page: Int): [Repository]
+  }
+`;
+
+export default typeDefs;
