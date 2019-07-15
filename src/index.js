2db2a915e6392d99a27555bbbf66662ce183e33e
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2019-07-15 20:12:43 +0100

    Add appolo provider and apollo hooks provider wrappers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+import {ApolloProvider as ApolloHooksProvider} from "react-apollo-hooks";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <ApolloHooksProvider client={client}>
+      <App />
+    </ApolloHooksProvider>
+  </ApolloProvider>,
+  document.getElementById("root"),
+);
+
 serviceWorker.unregister();
