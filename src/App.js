400bdb88ebd869fb4fa193baff8c00441dca936d
Imtiyaz-CHOUJAI <choujai.imtiyaz@gmail.com>
2019-07-15 20:14:18 +0100

    Use created components to display list of repositories and paginate infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import BackToTop from "react-back-to-top-button";
+import moment from "moment";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import RepositoriesListQuery from "./components/RepositoriesListQuery";
+import "./scss/app.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronCircleUp} from "@fortawesome/free-solid-svg-icons";
+import {Logo} from "./components/base/Logo";
+import {UseDebounce} from "./utils/UseDebounce";
+
+const App = () => {
+  const [searchCreatedAtInput, setSearchCreatedAtInput] = useState(
+    moment().format("YYYY-MM-DD"),
   );
-}
+
+  const [createdAtQueryVariable, setCreatedAtQueryVariable] = useState(
+    moment()
+      .subtract(30, "days")
+      .format("YYYY-MM-DD"),
+  );
+
+  const debouncedSearchCreatedAtInput = UseDebounce(searchCreatedAtInput, 1000);
+
+  useEffect(() => {
+    if (debouncedSearchCreatedAtInput) {
+      setCreatedAtQueryVariable(
+        moment(searchCreatedAtInput)
+          .subtract(30, "days")
+          .format("YYYY-MM-DD"),
+      );
+    }
+  }, [debouncedSearchCreatedAtInput]);
+
+  return (
+    <React.Fragment>
+      <Logo />
+      <div className="search">
+        <input
+          type="date"
+          className="search__input"
+          value={searchCreatedAtInput}
+          onChange={e => setSearchCreatedAtInput(e.target.value)}
+        />
+      </div>
+
+      <div className="container">
+        <RepositoriesListQuery createdAt={createdAtQueryVariable} />
+      </div>
+      <BackToTop
+        showOnScrollUp
+        showAt={100}
+        speed={1500}
+        easing="easeInOutQuint"
+      >
+        <FontAwesomeIcon icon={faChevronCircleUp} />
+      </BackToTop>
+    </React.Fragment>
+  );
+};
 
 export default App;
